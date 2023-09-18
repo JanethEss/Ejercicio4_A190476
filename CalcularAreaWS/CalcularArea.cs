202bfc49b3d8b95df4b7c406c815ea285eb67115
JanethEss <89121594+JanethEss@users.noreply.github.com>
2023-09-18 16:43:11 -0600

    Add files via upload

---
+++ CalcularAreaWS/CalcularArea.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalcularAreaWS
+{
+    public class CalcularArea
+    {
+        public double numero1 { get; set; }
+        public double numero2 { get; set; }
+
+        public double cuadrado(double lado1)
+        {
+            numero1 = lado1;           
+            var resultado = numero1 * numero1;
+            return resultado;
+        }
+        public double triangulo(double bse, double altura)
+        {
+            numero1 = bse;
+            numero2 = altura;
+            var resultado = (numero1 * numero2) / 2;
+            return resultado;
+        }
+        public double circulo(double radio)
+        {
+            numero1 = radio;            
+            var resultado = Math.PI* Math.Pow(numero1, 2);
+            return resultado;
+        }
+    }
+}
---
+++ CalcularAreaWS/CalcularWebService.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CalcularAreaWS
+{
+    /// <summary>
+    /// Descripción breve de CalcularWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+
+    //Practica 3 - Servicio Web en .NET
+    // Servicios web - 9°J
+    // Ramos Espinosa Claudia Janeth
+    public class CalcularWebService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public double areacuadrado(double lado1)
+        {
+            CalcularArea calcular = new CalcularArea();            
+            return calcular.cuadrado(lado1);
+        }
+        [WebMethod]
+        public double areatriangulo(double bse, double altura)
+        {
+            CalcularArea calcular = new CalcularArea();
+            return calcular.triangulo(bse, altura);
+        }
+        [WebMethod]
+        public double areacirculo(double radio)
+        {
+            CalcularArea calcular = new CalcularArea();
+            return calcular.circulo(radio);
+        }
+    }
+}
